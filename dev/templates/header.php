81c129ea819c359566cccf17f2a3c1cd1ae47744
v0ffk4 <v0ffk4@outlook.com>
2016-12-10 23:48:20 +0300

    Feat: add wp_head

--- dev/templates/header.php
+++ dev/templates/header.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <head>
-<meta charset="UTF-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1">
-
-
-<link href="<?php bloginfo(template_directory); ?>/style.css" rel="stylesheet">
-
-
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
+		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<?php endif; ?>
+	<?php wp_head(); ?>
+	<link href="<?php bloginfo(template_directory); ?>/style.css" rel="stylesheet">
 </head>
 
 <body class="<?php echo $class; ?>">
